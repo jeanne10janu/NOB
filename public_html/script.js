9eab69721a43b86d42488d4705cb80611c088b3a
tijmenbeemster <39950368+tijmenbeemster@users.noreply.github.com>
2018-06-26 23:34:19 +0200

    Update script.js

--- public_html/script.js
+++ public_html/script.js
@@ -4,18 +4,37 @@
 //var lengte = document.getElementById('lengte').value;
 //var alcohol = document.querySelector('input[name=alcohol]:checked').value;
 //var sigaretten = document.querySelector('input[name=sigaretten]:checked').value;
+const maxMedicijnenRecept = '56';
+const maxMedicijnenOTC = '07';
+const maxMedisch = '55';
+const maxDuikongevallen = '06';
+var gebJaarRegEx = new RegExp('19\d\d|2000$');
+
 var firebaseRef = firebase.database().ref();
-function btnTest() {
-    
-    radioToDb(firebaseRef, 'gender');
+
+function btnTest() {    
+    radioToDb(firebaseRef, 'geslacht');
     textToDb(firebaseRef, 'gebJaar');
-    arrayToDb(firebaseRef, )
+    checkboxToDb(firebaseRef, 'medicijnenRecept', maxMedicijnenRecept);
+    //window.open('inleiding.html', '_self');
 }
 
-function textToDb(dbRef, veldNaam){
+function validateForm(){
+    //console.log(checkInputNumeriek(minGebJaar, maxGebJaar, 'gebJaar'));
+    console.log(checkInput('gebJaar', gebJaarRegEx));
+    checkInput('gebJaar', gebJaarRegEx);
+    //return checkInputNumeriek(minGebJaar, maxGebJaar, 'gebJaar');
+}
+
+function textToDb(dbRef, veldNaam){    
     var value = document.getElementById(veldNaam).value;
-    dbRef.child(veldNaam).set(value);
-    
+    if(!checkInputNumeriek(minGebJaar, maxGebJaar, value)){
+        window.alert(veldNaam + ' moet een waarde zijn tussen ' + minGebJaar + ' en ' + maxGebJaar + '.' +
+                '\n Vul een geldige waarde in.');
+    }
+    else{
+        dbRef.child(veldNaam).set(value);  
+    }
 }
 
 function radioToDb(dbRef, radioNaam){
@@ -23,12 +42,12 @@ function radioToDb(dbRef, radioNaam){
     dbRef.child(radioNaam).set(value);
 }
 
-function checkboxToDb(dbRef, checkboxClass){
-    var array = getCheckedValuesArray(checkboxClass);
-    dbRef.child(checkboxClass).set(array);
+function checkboxToDb(dbRef, checkboxClass, maxWaarde){
+    var array = getCheckedValuesArray(checkboxClass, maxWaarde);
+    dbRef.child(checkboxClass).set([array]);
 }
 
-function getCheckedValuesArray(checkboxClass){
+function getCheckedValuesArray(checkboxClass, maxWaarde){
     var checkedValues = []; 
     var inputElements = document.getElementsByClassName(checkboxClass);
     for(var i=0; i < inputElements.length; ++i){
@@ -36,5 +55,21 @@ function getCheckedValuesArray(checkboxClass){
                checkedValues.push(inputElements[i].value);
           }
     }
+    if(checkedValues[checkedValues.length-1] === maxWaarde){
+        var value = document.getElementById(checkboxClass+'Tekst').value;
+        checkedValues.pop(checkedValues.length-1);
+        checkedValues.push(value);
+    }
     return checkedValues;
+}
+
+function checkInput(veldNaam, regex){
+    var value = document.getElementById(veldNaam).value;
+    /*var correctInput = true;
+    
+    if (!regex.test(value)){
+        correctInput = false;
+        console.log(regex.test(value));
+    }*/
+    return regex.test(value);   
 }
